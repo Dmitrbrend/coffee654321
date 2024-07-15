980f7b183de91d6be68fb44e2a7d22dcd512052b
Dmitrbrend <161335739+Dmitrbrend@users.noreply.github.com>
2024-07-15 13:23:37 +0300

    Add files via upload

--- coffee654321/coffee654321/coffe peace1/src/assets/js/script.js
+++ coffee654321/coffee654321/coffe peace1/src/assets/js/script.js
@@ -26,55 +26,6 @@ window.addEventListener("DOMContentLoaded", () => {
 
   bindModal(".btn", ".modal");
 
-  //slider
-
-  let slideIndex = 1;
-  const slides = document.querySelectorAll(".slide"),
-    prev = document.querySelector(".offer__slider-prev"),
-    next = document.querySelector(".offer__slider-next"),
-    total = document.querySelector("#total"),
-    current = document.querySelector("#current");
-
-  showSlides(slideIndex);
-
-  if (slides.length < 10) {
-    total.textContent = `0${slides.length}`;
-  } else {
-    total.textContent = slides.length;
-  }
-
-  function showSlides(n) {
-    if (n > slides.length) {
-      slideIndex = 1;
-    }
-    if (n < 1) {
-      slideIndex = slides.length;
-    }
-
-    slides.forEach((item) => (item.style.display = "none"));
-
-    slides[slideIndex - 1].style.display = "flex"; // Как ваша самостоятельная работа - переписать на использование классов show/hide
-
-    if (slides.length < 10) {
-      current.textContent = `0${slideIndex}`;
-    } else {
-      current.textContent = slideIndex;
-    }
-  }
-
-  function plusSlides(n) {
-    showSlides((slideIndex += n));
-  }
-
-  prev.addEventListener("click", function () {
-    plusSlides(-1);
-  });
-
-  next.addEventListener("click", function () {
-    plusSlides(1);
-  });
-
-
   //Добовление карточек экслюзивного меню 
 
   class MenuCard {
@@ -122,10 +73,58 @@ window.addEventListener("DOMContentLoaded", () => {
 
 getResource('http://localhost:3000/menu')
     .then(data => {
-        data.forEach(({img, altimg, title, descr, price}) => {
-            new MenuCard(img, altimg, title, descr, price, ".exclusive-coffee .container .slider-container .slider .").render();
+        data.forEach(({img, altimg, title, cost, price}) => {
+            new MenuCard(img, altimg, title, cost, price, ".exclusive-coffee .container .slider-container .slider").render();
         });
     });
    
+//slider
 
+
+    
+  let slideIndex = 1;
+  const slides = document.querySelectorAll(".slide"),
+    prev = document.querySelector(".offer__slider-prev"),
+    next = document.querySelector(".offer__slider-next"),
+    total = document.querySelector("#total"),
+    current = document.querySelector("#current");
+
+  showSlides(slideIndex);
+
+  if (slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+  } else {
+    total.textContent = slides.length;
+  }
+
+  function showSlides(n) {
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+
+    slides.forEach((item) => (item.style.display = "none"));
+
+    slides[slideIndex - 1].style.display = "flex"; // Как ваша самостоятельная работа - переписать на использование классов show/hide
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+  }
+
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+
+  prev.addEventListener("click", function () {
+    plusSlides(-1);
+  });
+
+  next.addEventListener("click", function () {
+    plusSlides(1);
+  });
 });
